01911edfa1207586f19bc0fd5e2195ff809f0059
rendratry <rendratrykusuma@gmail.com>
2023-10-20 14:45:23 +0700

    restful api ci4

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,10 +2,10 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
-
+use CodeIgniter\API\ResponseTrait;
 
 class ProductController extends BaseController {
-
+    use ResponseTrait;
     public function __construct(){
         $this->product = new ProductModel();
     }
@@ -26,12 +26,41 @@ class ProductController extends BaseController {
         return view('product', $data);
     }
 
+    // Restful API
+    public function readProductApi(){
+        $products = $this->product->findAll();
+        return $this->respond([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $products
+        ]);
+    }
+
     public function getProduct($id){
         $product = $this->product->where('id',$id)->first();
         $data = [
             'product' => $product
         ];
         return view('edit_product', $data);
+    }
+
+    public function getProductApi($id){
+        $product = $this->product->where('id',$id)->first();
+        if (!$product) {
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON([
+                'code' => 404,
+                'status' => 'NOT FOUND',
+                'data' => 'data tidak ditemukan'
+            ]);
+        }
+
+        return $this->respond([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $product
+        ]);
+
     }
 
     public function updateProduct($id){
